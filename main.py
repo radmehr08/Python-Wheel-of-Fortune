9a9ed29102e70ffe2e18e9975e15c50c04e51389
radmehr alizadeh <317630286+radmehr08@users.noreply.github.com>
2026-09-07 10:42:29 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,105 @@
+import random
+
+
+
+for_many = input("How many segments do you want your wheel of fortune to have( 2 to 10) ? ")
+for_many = int(for_many)
+
+if for_many == 2:
+    for_2_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_2_2 = input("Write down number you would like to be chosen at random2 : ")
+    random_1 = random.choice([for_2_1, for_2_2])
+    print(random_1)
+
+if for_many == 3:
+    for_3_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_3_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_3_3 = input("Write down number you would like to be chosen at random3 : ")
+    random_2 = random.choice([for_3_1, for_3_2, for_3_3])
+    print(random_2)
+
+
+if for_many == 4:
+    for_4_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_4_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_4_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_4_4 = input("Write down number you would like to be chosen at random4 : ")
+    random_3 = random.choice([for_4_1, for_4_2, for_4_3, for_4_4])
+    print(random_3)
+
+
+if for_many == 5:
+    for_5_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_5_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_5_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_5_4 = input("Write down number you would like to be chosen at random4 : ")
+    for_5_5 = input("Write down number you would like to be chosen at random5 : ")
+    random_4 = random.choice([for_5_1, for_5_2, for_5_3, for_5_4, for_5_5])
+    print(random_4)
+
+
+if for_many == 6:
+    for_6_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_6_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_6_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_6_4 = input("Write down number you would like to be chosen at random4 : ")
+    for_6_5 = input("Write down number you would like to be chosen at random5 : ")
+    for_6_6 = input("Write down number you would like to be chosen at random6 : ")
+    random_5 = random.choice([for_6_1, for_6_2, for_6_3, for_6_4, for_6_5, for_6_6])
+    print(random_5)
+
+
+
+if for_many == 7:
+    for_7_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_7_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_7_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_7_4 = input("Write down number you would like to be chosen at random4 : ")
+    for_7_5 = input("Write down number you would like to be chosen at random5 : ")
+    for_7_6 = input("Write down number you would like to be chosen at random6 : ")
+    for_7_7 = input("Write down number you would like to be chosen at random7 : ")
+    random_6 = random.choice([for_7_1, for_7_2, for_7_3, for_7_4, for_7_5, for_7_6, for_7_7])
+    print(random_6)
+
+
+
+if for_many == 8:
+    for_8_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_8_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_8_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_8_4 = input("Write down number you would like to be chosen at random4 : ")
+    for_8_5 = input("Write down number you would like to be chosen at random5 : ")
+    for_8_6 = input("Write down number you would like to be chosen at random6 : ")
+    for_8_7 = input("Write down number you would like to be chosen at random7 : ")
+    for_8_8 = input("Write down number you would like to be chosen at random7 : ")
+    random_7 = random.choice([for_8_1, for_8_2, for_8_3, for_8_4, for_8_5, for_8_6, for_8_7, for_8_8])
+    print(random_7)
+
+
+if for_many == 9:
+    for_9_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_9_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_9_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_9_4 = input("Write down number you would like to be chosen at random4 : ")
+    for_9_5 = input("Write down number you would like to be chosen at random5 : ")
+    for_9_6 = input("Write down number you would like to be chosen at random6 : ")
+    for_9_7 = input("Write down number you would like to be chosen at random7 : ")
+    for_9_8 = input("Write down number you would like to be chosen at random7 : ")
+    for_9_9 = input("Write down number you would like to be chosen at random7 : ")
+    random_8 = random.choice([for_9_1, for_9_2, for_9_3, for_9_4, for_9_5, for_9_6, for_9_7, for_9_8, for_9_9])
+    print(random_8)
+
+
+if for_many == 10:
+    for_10_1 = input("Write down number you would like to be chosen at random1 : ")
+    for_10_2 = input("Write down number you would like to be chosen at random2 : ")
+    for_10_3 = input("Write down number you would like to be chosen at random3 : ")
+    for_10_4 = input("Write down number you would like to be chosen at random4 : ")
+    for_10_5 = input("Write down number you would like to be chosen at random5 : ")
+    for_10_6 = input("Write down number you would like to be chosen at random6 : ")
+    for_10_7 = input("Write down number you would like to be chosen at random7 : ")
+    for_10_8 = input("Write down number you would like to be chosen at random7 : ")
+    for_10_9 = input("Write down number you would like to be chosen at random7 : ")
+    for_10_10 = input("Write down number you would like to be chosen at random7 : ")
+    random_9 = random.choice([for_10_1, for_10_2, for_10_3, for_10_4, for_10_5, for_10_6, for_10_7, for_10_8, for_10_9, for_10_10])
+    print(random_9)
